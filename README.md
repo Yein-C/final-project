b77813c9ca4f91f85e27c7a7fdb85fe7bc879739
Yein <yein466@gmail.com>
2020-06-09 18:42:02 +0100

    미완3

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
  이 프로젝트는 기기가 과열될 때 자동으로 꺼질 수 있도록 하는 프로젝트입니다.
 
- 설치전 필수 Check
-1.
-2.
+주의!
+=====
 
- 작동법
-1.
-2.
-3.
-4.
-5.
+반드시 home에 위치한 상태에서 clone하세요.
+
+시작 전 필수
+------------
+sudo apt-get update
+sudo apt-get upgrade
+
+있으면 좋은 것: sudo apt-get vim
 
 
+1. git clone
+-------------
+cd ~ 이나 cd
+git clone https://github.com/Yein-C/final-project.git
--- main.cpp
+++ main.cpp
@@ -14,18 +14,21 @@ int main() {
 	cout<<"CPU thermometer start"<<endl;
 
 	system("cat /sys/class/thermal/thermal_zone0/temp > cpu.txt");
-	
 	fc.open("cpu.txt");
+
 	getline(fc, cpu);
+	
 	cpu_tem=stod(cpu);
 	cpu_tem/=1000;
 	if(cpu_tem>=70){
+		system("aplay /usr/share/sounds/alsa/Front_Center.wav");
 		cout<<"cpu: 70'c (96%)."<<endl;
 		cout<<"SHUTDOWN after 3s"<<endl;
 		system("sleep(3)");
 		system("sudo shutdown -h now");
 	}
 	else if(cpu_tem>=65){
+		system("aplay /usr/share/sounds/alsa/Rear_Left.wav");
 		cout<<"cpu: 65'c (89%). WARNING"<<endl;
 		cout<<"SHUTDOWN after 1m"<<endl;
 		system("sudo shutdown -h 1");
@@ -33,21 +36,28 @@ int main() {
 		cout<<"  sudo shutdown -c  "<<endl;
 	}
 	else if(cpu_tem>=60){
+		system("aplay /usr/share/sounds/alsa/Side_Right.wav");
 		cout<<"cpu: 60'c (82%)."<<endl;
 		cout<<"Maybe turned off while in use"<<endl;
 	}
 	else if(cpu_tem>=55){
+		system("aplay /usr/share/sounds/alsa/Front_Left.wav");
 		cout<<"cpu: 55'c"<<endl;
 		cout<<"Why don't you turn off the RASPBERRY PI and take a break?"<<endl;
 	}
 	else if(cpu_tem>=40){
+		system("aplay /usr/share/sounds/alsa/Noise.wav");
 		cout<<"55'c~40'c  testing"<<endl;
 	}
+
 	
 	if(cpu_tem<=0){
+		system("aplay /usr/share/sounds/alsa/Side_Left.wav");
 		cout<<"0'c. SHUTDOWN after 1m"<<endl;
 		system("sudo shutdown -h 1");
 	}
 	
+	cout<<"cpu: "<<cpu_tem<<"'c"<<endl;
+	system("mv ~/cpu.txt ~/final-project/");
 	return 0;
 }
